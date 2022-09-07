48dd284a53ed12e53fd77c784812592e94a1062d
miliadou <E63488@nbg.gr>
2022-09-07 16:00:17 +0300

    Rename table column fields

--- LoggingTemplate/Controllers/LoggerHelper.cs
+++ LoggingTemplate/Controllers/LoggerHelper.cs
@@ -8,28 +8,28 @@ namespace RestAPIwithSOAPIntegration.Controllers
 {
     public static class LoggerHelper
     {
-        public static void HeadTableLogging(Guid guid, string order_type, string order_status)
+        public static void HeadTableLogging(Guid global_uuid, string use_case, string order_status)
         {
             var loggerH = LogManager.GetLogger("Head_Table");
 
             LogEventInfo headLogEvent = new LogEventInfo(LogLevel.Info, "Head_Table","");
-            headLogEvent.Properties.Add("GUID", guid);
-            headLogEvent.Properties.Add("OrderType", order_type);
+            headLogEvent.Properties.Add("GlobalUuid", global_uuid);
+            headLogEvent.Properties.Add("UseCase", use_case);
             headLogEvent.Properties.Add("OrderStatus", order_status);
 
             loggerH.Info(headLogEvent);
         }
 
-        public static void DetailsTableLogging(Guid guid, Guid uuid, string order_type, string service, string end_system, string state, object payload, object ex)
+        public static void DetailsTableLogging(Guid global_uuid, Guid request_uuid, string use_case, string service, string end_system, string state, object payload, object ex)
         {
             var loggerD = LogManager.GetLogger("Details_Table");
 
             if (state == "Error")
             {
                 LogEventInfo detailsLogEvent = new LogEventInfo(LogLevel.Error, "Details_Table", null, "", new List<object>().ToArray(), (Exception)ex);
-                detailsLogEvent.Properties.Add("GUID", guid);
-                detailsLogEvent.Properties.Add("OrderType", order_type);
-                detailsLogEvent.Properties.Add("UUID", uuid);
+                detailsLogEvent.Properties.Add("GlobalUuid", global_uuid);
+                detailsLogEvent.Properties.Add("UseCase", use_case);
+                detailsLogEvent.Properties.Add("RequestUuid", request_uuid);
                 detailsLogEvent.Properties.Add("Service", service);
                 detailsLogEvent.Properties.Add("EndSystem", end_system);
                 detailsLogEvent.Properties.Add("State", state);
@@ -39,9 +39,9 @@ namespace RestAPIwithSOAPIntegration.Controllers
             else 
             {
                 LogEventInfo detailsLogEvent = new LogEventInfo(LogLevel.Info, "Details_Table", "");
-                detailsLogEvent.Properties.Add("GUID", guid);
-                detailsLogEvent.Properties.Add("OrderType", order_type);
-                detailsLogEvent.Properties.Add("UUID", uuid);
+                detailsLogEvent.Properties.Add("GlobalUuid", global_uuid);
+                detailsLogEvent.Properties.Add("UseCase", use_case);
+                detailsLogEvent.Properties.Add("RequestUuid", request_uuid);
                 detailsLogEvent.Properties.Add("Service", service);
                 detailsLogEvent.Properties.Add("EndSystem", end_system);
                 detailsLogEvent.Properties.Add("State", state);
